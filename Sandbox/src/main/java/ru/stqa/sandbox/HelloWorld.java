7fb6aebbb3647210c34719a624de6f9777cbee35
Max <illogical.180@gmail.com>
2019-07-09 16:17:55 +0300

    functions

--- Sandbox/src/main/java/ru/stqa/sandbox/HelloWorld.java
+++ Sandbox/src/main/java/ru/stqa/sandbox/HelloWorld.java
@@ -1,7 +1,24 @@
 package ru.stqa.sandbox;
 
 public class HelloWorld {
+
 public static void main (String[] args) {
-System.out.println("Hello world!");
+hello("Worlds");
+hello("Seas");
+double l =5;
+    System.out.println("Площадь квадрата со стороной " +l+ " равна " +area(l));
+    double a = 6;
+    double b = 5;
+    System.out.println("Площадь прямоугольника со стороной " +a+ " и " +b+ " равна " +area(a,b));
+
+}
+public static void hello(String somebody ){
+    System.out.println("Hello " + somebody);
+}
+public static double area(double len) {
+    return len*len;
+}
+public static double area(double a, double b){
+    return a*b;
 }
 }
