96586395f76cceb47a6ac64dc3b823d1ab5e929f
Max <illogical.180@gmail.com>
2019-07-11 15:49:33 +0300

    More condition checks and tests for equation

--- Sandbox/src/test/java/ru/stqa/sandbox/EquationTest.java
+++ Sandbox/src/test/java/ru/stqa/sandbox/EquationTest.java
@@ -17,6 +17,18 @@ public class EquationTest {
         Equation e = new Equation(1,5,6);
         Assert.assertEquals(e.rootNumber(),2);
     }
+    public void linear(){
+        Equation e = new Equation(0,1,1);
+        Assert.assertEquals(e.rootNumber(),1);
+    }
+    public void constant(){
+        Equation e = new Equation(0,0,1);
+        Assert.assertEquals(e.rootNumber(),0);
+    }
+    public void zeroTest(){
+        Equation e = new Equation(0,0,0);
+        Assert.assertEquals(e.rootNumber(),-1);
+    }
 }
 
 
